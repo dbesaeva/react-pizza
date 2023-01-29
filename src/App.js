8b34488aab7ad1651d27928ba25451f6911b86e9
dbesaeva <daliyabesaeva1@gmail.com>
2023-01-29 13:12:45 +0700

    add header layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="name d-flex align-center">
+            <div>
+                <h3>PizzaBro</h3>
+            </div>  
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30 cu-p">
+              <img width={23} height={23} src="img/shopping-bag.svg" alt="Cart" />
+              <span>1300 руб.</span>
+          </li>
+          <li className='mr-20 cu-p'>
+                <img className="mr-30 cu-p" width={23} height={23} src="img/favorite.svg" alt="Favorites" />
+          </li>
+              <img width={19} height={19} src="img/user.svg" alt="User" />
+        </ul>
       </header>
     </div>
   );
